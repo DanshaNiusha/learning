6ab871d09dc84897a1a497cd030892f23ac597c7
liuxiaokang03 <liuxiaokang03@meituan.com>
2021-04-29 20:55:24 +0800

    optional

--- src/main/java/test/TreeTest.java
+++ src/main/java/test/TreeTest.java
@@ -1,6 +1,13 @@
 package test;
 
+import com.google.common.collect.Lists;
 import lombok.ToString;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 /**
  * @author liuxiaokang
@@ -27,20 +34,58 @@ public class TreeTest {
     }
     
     public static void main(String[] args) {
-        Father father = new Father();
-        father.name = "ren";
-        father.sex = "1";
-        Child child = new Child();
-        child.name = "child";
-        child.genfer= "das";
-        child.sex  ="2";
-    
-        Father f = child;
-        System.out.println(f);
-        Father fa = child;
-        System.out.println(fa);
+        // Father father = new Father();
+        // father.name = "ren";
+        // father.sex = "1";
+        //
+        // ArrayList<String> list = Lists.newArrayList("1", "2");
+        // ArrayList<String> list1 = deepClone(list);
+        // list.remove("1");
+        // System.out.println(list1);
+        // System.out.println((3&1));
+        // System.out.println((3&2));
+        // System.out.println((3&3));
+        // System.out.println((3&4));
+        // System.out.println((3&5));
+        // System.out.println((3&6));
+        // System.out.println((3&7));
+        
+        
+        System.out.println(2|0|0|0);
+        System.out.println(2|1);
+        // Child child = new Child();
+        // child.name = "child";
+        // child.genfer= "das";
+        // child.sex  ="2";
+        //
+        // Father f = child;
+        // System.out.println(f);
+        // Father fa = child;
+        // System.out.println(fa);
+        // System.out.println(15|5);
     
     }
+    
+    
+    /**
+     * 深拷贝工具
+     * @author liuxiaokang
+     * @date 2021/4/25
+     */
+    public static <T> T deepClone(T source) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(source);
+            ByteArrayInputStream bis = new ByteArrayInputStream(baos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            return (T) ois.readObject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    
     @ToString
     static class Father {
         public String name;
